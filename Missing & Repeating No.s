3a63b15cfcb38b941a8542cf0f88c4d135d86514
Anuradha Naidu <88324015+Anuradha-Naidu@users.noreply.github.com>
2023-06-16 11:01:02 +0530

    Update Missing & Repeating No.s

--- Missing & Repeating No.s
+++ Missing & Repeating No.s
@@ -23,4 +23,35 @@ pair<int, int> missingAndRepeating(vector<int>& arr, int n) {
 
     return {missing, repeating};
 }
+........................................................................................................................................................................
+// optimal soln #1 (Maths) : TC = O(n), SC = O(1)
+
+#include <bits/stdc++.h>
+
+pair<int,int> missingAndRepeating(vector<int> &arr, int n)
+{   
+	//long long n = arr.size();
+	
+	long long SN = (n * (n+1))/2;
+	long long S2N = (n * (n+1) * (2*n + 1))/6;
+	
+	long long S = 0, S2 = 0;
+
+	for(int i =0; i<n; i++){
+		S += arr[i];
+		S2 += (long long)arr[i] * (long long)arr[i];
+	}
+	
+	long long val1 = S - SN;
+	long long val2 = S2 - S2N;
+	
+	val2 = val2 / val1;
+
+	long long x = (val1 + val2)/2; 
+	long long y = x - val1;
+
+	return{(int)y, (int)x};
+	
+}
+
 
